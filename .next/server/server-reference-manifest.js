129164d078ba542ed9eb5edb430001d03d239de8
Samuel Muniz <137716288+DevSamuelMuniz@users.noreply.github.com>
2025-04-13 14:33:14 -0300

    ajustes

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"dL2Ojik2WlIGR9wIJdlbzHvEtfOYsMvhgJFzRXHz64A=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
